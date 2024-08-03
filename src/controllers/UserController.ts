20a5f225f11244d3875ec9f35cdddee6c6015e02
Rushabh Shah <rushabh.shah@powerschool.com>
2024-08-03 10:53:46 +0530

    Added routing controllers

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,30 @@
+import { JsonController, Param, Body, Get, Post, Put, Delete } from 'routing-controllers';
+
+@JsonController()
+export class UserController {
+
+  @Get('/users')
+  getAll() {
+    return 'This action returns all users';
+  }
+
+  @Get('/users/:id')
+  getOne(@Param('id') id: number) {
+    return 'This action returns user #' + id;
+  }
+
+  @Post('/users')
+  post(@Body() user: any) {
+    return `Saving user... ${JSON.stringify(user)}`;
+  }
+
+  @Put('/users/:id')
+  put(@Param('id') id: number, @Body() user: any) {
+    return `Updating a user... ${id}`;
+  }
+
+  @Delete('/users/:id')
+  remove(@Param('id') id: number) {
+    return `Removing user... ${id}`;
+  }
+}
