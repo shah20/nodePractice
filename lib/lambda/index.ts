32b8320a1ff7806891122007c072e3420acb8639
Rushabh Shah <rushabh.shah@powerschool.com>
2024-08-05 15:41:47 +0530

    Wired Application code to lambda

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -1,9 +1,9 @@
-// import { LambdaHandler } from '../../src/index';
+import { LambdaHandler } from '../../src/index';
 
 export async function applicationHandler(event: any, context: any): Promise<unknown> {
   console.log('applicationHandler', event);
-  // const _handler = await LambdaHandler();
-  // const result = await _handler(event, context);
+  const _handler = await LambdaHandler();
+  const result = await _handler(event, context);
 
-  return "result";
+  return result;
 }
