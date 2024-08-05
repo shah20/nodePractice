b83f7b8610bdbb2aaeef6406e0f64ea55295ce08
Rushabh Shah <rushabh.shah@powerschool.com>
2024-08-05 10:21:39 +0530

    Added Application Lambda

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,17 +6,6 @@ import config from 'config';
 
 const app = new cdk.App();
 new PracticeStack(app, 'PracticeStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
   env: { account: config.get('deployment.account'), region: config.get('deployment.region') },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
