c46f6df8b1ea17d1e724961bb9f2583ea830a3b0
Rushabh Shah <rushabh.shah@powerschool.com>
2024-08-13 16:51:38 +0530

    User CRUD
    Added Get All Users, get userbyID, delete user, update user

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,7 +1,8 @@
 import { ConditionalCheckFailedException } from "@aws-sdk/client-dynamodb";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DeleteCommand, GetCommand, PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import Container, { Service } from "typedi";
 import { DBHelperService } from "./DBHelperService";
+import c from "config";
 
 const USERS_TABLE = process.env.USERS_TABLE;
 
@@ -14,21 +15,22 @@ export class UserService {
     this.dbHelperService = Container.get(DBHelperService);
   }
 
-  async createUser(user: any) {
+  async createUpdateUser(user: any, id?: string) {
     const putCommand = new PutCommand({
       TableName: USERS_TABLE,
       Item: {
-        id: (await this.dbHelperService.getNewId(USERS_TABLE!)).toString(),
+        id: id || (await this.dbHelperService.getNewId(USERS_TABLE!)).toString(),
         name: user.name,
         email: user.email,
       },
       // By default PutItem overrides the record if same Partition key exists
       // By providing below condition it would throw error if Partition key already exists
-      ConditionExpression: 'attribute_not_exists(id)',
+      ConditionExpression: id ? undefined : 'attribute_not_exists(id)',
     });
+    console.log('Creating a user...', { putCommand: JSON.stringify(putCommand)});
     try {
       const result = await this.dbHelperService.getResult(putCommand);
-      return { result};
+      return { result };
     } catch (error) {
       if (error instanceof ConditionalCheckFailedException) {
         const errorMessage = `Item with the ${user.id} partition key already exists.`;
@@ -37,6 +39,52 @@ export class UserService {
         console.error("An error occurred:", error);
         throw error;
       }
+    }
+  }
+
+  async getAllUsers() {
+    const scanCommand = new ScanCommand({
+      TableName: USERS_TABLE!,
+    });
+    try {
+      const result = await this.dbHelperService.getResult(scanCommand);
+      return { result };
+    } catch (error) {
+      console.error("An error occurred:", error);
+      throw error;
+    }
+  }
+  
+  async getUserByid(id: string) {
+    const getCommend = new GetCommand({
+      TableName: USERS_TABLE!,
+      Key: {
+        id: id,
+      },
+    });
+    try {
+      const result = await this.dbHelperService.getResult(getCommend);
+      return { result };
+    } catch(error) {
+      console.error("An error occurred:", error);
+      throw error;
+    }
+  }
+
+  async deleteUser(id: string) {
+    const deleteCommand = new DeleteCommand({
+      TableName: USERS_TABLE!,
+      Key: {
+        id,
+      },
+      ReturnValues: 'ALL_OLD',
+    });
+    try {
+      const result = await this.dbHelperService.getResult(deleteCommand);
+      return { result };
+    } catch (error) {
+      console.error("An error occurred:", error);
+      throw error;
     }
   }
 }
